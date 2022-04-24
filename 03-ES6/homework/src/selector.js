0c329288b60e04963d7e39bac94fc4eab2c41091
Mauricio <madegasperis@gmail.com>
2022-04-24 13:05:25 -0300

    Homework ES6

--- 03-ES6/homework/src/selector.js
+++ 03-ES6/homework/src/selector.js
@@ -9,6 +9,15 @@ var traverseDomAndCollectElements = function(matchFunc, startEl) {
   // usa matchFunc para identificar elementos que matchien
 
   // TU CÓDIGO AQUÍ
+  if(matchFunc(startEl)) resultSet.push (startEl);
+
+  for(let i = 0; i < startEl.children.length; i++) {
+    let child = startEl.children[i];
+    let collected = traverseDomAndCollectElements(matchFunc, child);
+    resultSet = [...resultSet, ...collected];
+  }
+
+  return resultSet;
   
 };
 
@@ -18,7 +27,14 @@ var traverseDomAndCollectElements = function(matchFunc, startEl) {
 
 var selectorTypeMatcher = function(selector) {
   // tu código aquí
-  
+  if (selector.charAt(0) === "#"){
+    return "id";
+  } else if (selector.charAt(0) === "."){
+    return "class";
+  } else if (selector.indexOf(".") == -1){
+    return "tag";
+  } 
+  return "tag.class";
 };
 
 // NOTA SOBRE LA FUNCIÓN MATCH
@@ -29,13 +45,31 @@ var selectorTypeMatcher = function(selector) {
 var matchFunctionMaker = function(selector) {
   var selectorType = selectorTypeMatcher(selector);
   var matchFunction;
-  if (selectorType === "id") { 
-   
+  if (selectorType === "id") {
+    
+    matchFunction = element => "#" + element.id ===selector;
+
   } else if (selectorType === "class") {
+
+    matchFunction= element => {
+      let classes = element.classList;
+      for (let i =0; i < classes.length; i++){
+        if("." + classes[i] === selector) 
+        return true;
+      }
+      return false;
+    }
     
   } else if (selectorType === "tag.class") {
+
+    matchFunction = element => {
+      let [t, c] = selector.split(".");
+      return matchFunctionMaker(t)(element) && matchFunctionMaker("." + c)(element)
+    }
     
   } else if (selectorType === "tag") {
+
+    matchFunction = element => element.tagName === selector.toUpperCase();
     
   }
   return matchFunction;
