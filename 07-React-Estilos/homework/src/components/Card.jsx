04417cae3dc0d44cd1570c8a418596f2ec2190b4
Mauricio <madegasperis@gmail.com>
2022-05-05 20:59:05 -0300

    Homework React-Estilos

--- 07-React-Estilos/homework/src/components/Card.jsx
+++ 07-React-Estilos/homework/src/components/Card.jsx
@@ -1,6 +1,24 @@
 import React from 'react';
+import s from './Card.module.css';
 
 export default function Card(props) {
   // acá va tu código
-  return <div>Card Component</div>
-};
+  
+  return (
+    <div className={s.wrapper}>
+        <button onClick={props.onClose} className={s.cardBtn}>X</button>
+        <h3>{props.name}</h3>
+      <div className={s.tempCont}>
+        <div> 
+          <h6>Min</h6>
+          <p>{props.min}°</p>
+        </div>
+        <div>
+          <h6>Max</h6>
+          <p>{props.max}°</p>
+        </div>  
+          <img src= {`http://openweathermap.org/img/wn/${props.img}@2x.png`}/>     
+      </div> 
+    </div>
+    )
+  };
--- 07-React-Estilos/homework/src/components/Cards.jsx
+++ 07-React-Estilos/homework/src/components/Cards.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import Card from './Card.jsx';
+import s from './Cards.module.css'
 
 export default function Cards(props) {
   // acá va tu código
   // tip, podés usar un map
-  return <div>Cards Component</div>
+  return (
+  <div className={s.contenedor}>
+    {props.cities.map(c => (
+        <Card
+          key={c.id}
+          max={c.main.temp_max}
+          min={c.main.temp_min}
+          name={c.name}
+          img={c.weather[0].icon}
+          onClose={() => alert("Are you sure you want to delete " + c.name + "?")}
+         /> 
+      ))}
+    </div>
+  ) 
 };
